0c66d5d3390a32ceac52a07f39acc1f04f119c0d
RafGiammario <raf.giammario@gmail.com>
2021-06-10 12:24:34 +0200

    Laravel Vue

--- live-coding/vue/vue-cli/src/main.js
+++ live-coding/vue/vue-cli/src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import AxiosPlugin from './plugins/AxiosPlugin'
 
 Vue.config.productionTip = false
+
+Vue.use(AxiosPlugin)
 
 new Vue({
   render: h => h(App),
